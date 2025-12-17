a6d96120e80fe272bc7a8a163577f947bdf48438
Thong Nguyen <giathong3012005@gmail.com>
2025-12-17 11:28:07 -0700

    add main.py

--- APLC final/main.py
+++ APLC final/main.py
@@ -1,4 +1,44 @@
 from Vehicle import Vehicle
 
+def load_data(sampledata):
+    with open(sampledata, "r") as file:
+        return json.load(file)
+
 def main():
-    pass
+    try:
+        data = load_data("sampledata.json")
+
+        print("*******Parking system*******")
+        while True:
+            print("\nChoose an option:")
+            print("1. Assign new car")
+            print("2. Release a car")
+            print("3. Exit")
+
+            option = input("Enter your option: ").strip()
+
+            if option == "1":
+                plate = input("Enter license plate: ").strip().upper()
+                size = input("Enter vehicle size (compact/large): ").strip().lower()
+
+                vehicle = Vehicle(plate, size)
+                parking_lot.assign_spot(vehicle)
+
+            elif option == "2":
+                spotid = input("Enter parking spot ID to release: ").strip()
+                parkinglot.release_spot(spotid)
+
+            elif option == "3":
+                print("Exiting...")
+                break
+
+            else:
+                print("Invalid option. Tr again.")
+    except ParkingException as e:
+        print(f"Parking error: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()

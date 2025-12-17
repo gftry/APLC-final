2085366a145f3c828e5efddccb3b57bf592c717c
Troy <117055051+gftry@users.noreply.github.com>
2025-12-17 10:41:31 -0700

    Add custom exceptions for parking-related errors

--- APLC final/Exceptions.py
+++ APLC final/Exceptions.py
@@ -1,1 +1,15 @@
+class parkingeerror(Exception):
+    """Base class for parking-related exceptions."""
+    
+class invalidvehicleerror(parkingeerror):
+    """Exception raised for invalid vehicle types."""
+pass
+
+class invalidspoterror(parkingeerror):
+    """Exception raised for invalid spot types."""
+pass
+
+class parkingfullerror(parkingeerror):
+    """Exception raised when the parking lot is full."""
+pass
 

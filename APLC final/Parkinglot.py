bc00b1b4b87bccf575905fc2d47ae789099d9818
Tristin Bates <Tristin.Bates@edu.sait.ca>
2025-12-17 11:51:44 -0700

    Put parkingLot into a class

--- APLC final/Parkinglot.py
+++ APLC final/Parkinglot.py
@@ -1,29 +1,30 @@
-from Exceptions import invalidspoterror, parkingfullerror
 import json
 from json import dumps
 
-file_path = 'APLC final\sampledata.json'
+file_path = 'APLC final/sampledata.json'
 
 # load json file
-try:
-    with open(file_path, 'r') as data:
-        data_dict = json.load(data)
+def parkingLot():
 
-except FileNotFoundError:
-    print(f"File {file_path} doesn't exist")
+    try:
+        with open(file_path, 'r') as data:
+            data_dict = json.load(data)
 
-# parse data to find available spots for each vehicle
-for vehicle in data_dict['vehicles']:
-    parked = False
+    except FileNotFoundError:
+        print(f"File {file_path} doesn't exist")
 
-    for spot in data_dict['parkingSpots']:
-        if spot['available'] and vehicle['size'] == spot['size']:
+    # parse data to find available spots for each vehicle
+    for vehicle in data_dict['vehicles']:
+        parked = False
+
+        for spot in data_dict['parkingSpots']:
+            if spot['available'] and vehicle['size'] == spot['size']:
             
-            print(f"Spot {spot['id']} is available for {vehicle['plate']}")
+                print(f"Spot {spot['id']} is available for {vehicle['plate']}")
             
-            spot['available'] = False  
-            parked = True
-            break
+                spot['available'] = False  
+                parked = True
+                break
 
-    if not parked:
-        print(f"No available spot for vehicle {vehicle['plate']}")
+        if not parked:
+            print(f"No available spot for vehicle {vehicle['plate']}")
